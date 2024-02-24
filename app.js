1be8f594dc1cd55529e0ab0f647478803fe64cbf
J-ON-AS <118046576+J-ON-AS@users.noreply.github.com>
2024-02-24 20:21:17 +0530

    sth

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use(express.json());
 // extra packages
 
 // routes
+app.get('/',(req,res)=>{
+  res.send('Jobs Api')
+})
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/jobs',authenticateUser,jobsRouter);
 
